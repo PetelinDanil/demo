39fb5bfbc920cc336b10c20743258b8667ffb7f0
Danil <url.danil21@gmail.com>
2022-08-12 09:07:35 +0300

    add test oracle connection

--- src/test/java/jdbc/TestJdbc.java
+++ src/test/java/jdbc/TestJdbc.java
@@ -44,4 +44,42 @@ public class TestJdbc {
         }
 
     }
+
+    static final String DB_URL_ORACLE = "jdbc:oracle:thin:@192.168.0.55:1521:A";
+    static final String USER_ORACLE = "hr";
+    static final String PASS_ORACLE = "hr";
+
+
+    @Test
+    void testJdbc() {
+        System.out.println("--- Testing connection to Oracle JDBC");
+
+        Connection con = null;
+
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con = DriverManager
+                    .getConnection(DB_URL_ORACLE, USER_ORACLE, PASS_ORACLE);
+
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("select e.employee_id  empid, e.first_name || ' ' || e.last_name  name from employees e");
+            while(rs.next()){
+                System.out.println("Employee ID="+rs.getInt("empid")+", Name="+rs.getString("name"));
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                if(rs != null) rs.close();
+                if(stmt != null) stmt.close();
+                if(con != null) con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
